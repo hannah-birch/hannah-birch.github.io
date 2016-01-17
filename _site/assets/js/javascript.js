b89a13f1367448de81d581652297a46cbec3a28a
Hannah Birch <birch.hannah@gmail.com>
2016-01-17 17:43:29 -0500

    Starts working with .each solution

--- _site/assets/js/javascript.js
+++ _site/assets/js/javascript.js
@@ -1,18 +1,17 @@
-
-$(function() {
-	$('.site-nav .trigger a').on('click', function(e) {
-		e.preventDefault();
-		$('.site-nav .trigger a').removeClass('active');
-		$(this).addClass('active');
-	});
-});
+$(".trigger a").each(function() {
+  var link = this.pathname;
+  if (link == window.location.pathname) {
+    $(this).addClass("active")
+  }
+})
 
 
 
-//For adding active class to nav items
-// $(function() { 
-//     $('.site-nav').on('click','.trigger a', function (e) {
-//         e.preventDefault();
-//         $(this).parents('.site-nav').find('.active').removeClass('active').end().end().addClass('active');
-//     });
+
+// $(function() {
+// 	$('.site-nav .trigger a').on('click', function(e) {
+// 		e.preventDefault();
+// 		$('.site-nav .trigger a').removeClass('active');
+// 		$(this).addClass('active');
+// 	});
 // });
