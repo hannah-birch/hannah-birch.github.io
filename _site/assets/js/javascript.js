ae053dfa5b7ebf3cb9da8bcbbbc588c5385c3cca
Hannah Birch <birch.hannah@gmail.com>
2016-01-17 13:22:13 -0500

    More work on jquery nav toggle grr

--- _site/assets/js/javascript.js
+++ _site/assets/js/javascript.js
@@ -1,7 +1,18 @@
-//For adding active class to nav items
-$(function() { 
-    $('.site-nav').on('click','.trigger a', function ( e ) {
-        e.preventDefault();
-        $(this).parents('.site-nav').find('.active').removeClass('active').end().end().addClass('active');
-    });
+
+$(function() {
+	$('.site-nav .trigger a').on('click', function() {
+		$('.site-nav .trigger a').removeClass('active');
+		$(this).addClass('active');
+		return false;
+	});
 });
+
+
+
+//For adding active class to nav items
+// $(function() { 
+//     $('.site-nav').on('click','.trigger a', function (e) {
+//         e.preventDefault();
+//         $(this).parents('.site-nav').find('.active').removeClass('active').end().end().addClass('active');
+//     });
+// });
