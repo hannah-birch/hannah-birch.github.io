b732aaef28aefd2868d8e03f3101d86bb545393c
Hannah Birch <birch.hannah@gmail.com>
2020-04-06 21:12:22 -0400

    Swaps in Adactio's JS/CSS method

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,20 +1,31 @@
-$(document).ready(function(){
-    // Toggles span display
-    $(".toggle-button").click(function(){
-        $("span.transform").toggle();
-    });
+// https://css-tricks.com/snippets/javascript/loop-queryselectorall-matches/
+var forEach = function (array, callback, scope) {
+  for (var i = 0; i < array.length; i++) {
+    callback.call(scope, i, array[i]); // passes back stuff we need
+  }
+};
+
+var adjectives = document.querySelectorAll(".intro a");
+
+forEach(adjectives, function(i, el) {
+  el.addEventListener("click", function() {
+    document.querySelector(".intro a.active").classList.remove("active");
+    this.classList.add("active");
+  })
 });
 
-$(document).ready(function(){
-    // Toggles span display
-    $(".toggle-button").click(function(){
-        $("span.transform-2").toggle();
-    });
-});
 
-$(document).ready(function(){
-    // Toggles span display
-    $(".toggle-button").click(function(){
-        $("span.transform-3").toggle();
-    });
-});
+// Works for class toggle
+// $(document).ready(function(){
+//     // Toggles span display
+//     $(".toggle-button").click(function(){
+//         $("span.transform").toggle();
+//     });
+// });
+//
+// $(document).ready(function(){
+//     // Toggles span display
+//     $(".toggle-button").click(function(){
+//         $("span.transform-2").toggle();
+//     });
+// });
