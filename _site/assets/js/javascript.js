66fd1aec206d082aa8e6b4456ba5e8a3b013f1ae
Hannah Birch <birch.hannah@gmail.com>
2020-04-09 10:13:24 -0400

    Starts new JavaScript method, changes markup so all words will be visible all the time

--- _site/assets/js/javascript.js
+++ _site/assets/js/javascript.js
@@ -1,31 +1,27 @@
-// https://css-tricks.com/snippets/javascript/loop-queryselectorall-matches/
-var forEach = function (array, callback, scope) {
-  for (var i = 0; i < array.length; i++) {
-    callback.call(scope, i, array[i]); // passes back stuff we need
-  }
-};
-
-var adjectives = document.querySelectorAll(".intro a.active");
-
-forEach(adjectives, function(i, el) {
-  el.addEventListener("click", function() {
-    document.querySelector(".intro a.active").classList.remove("active");
-    this.classList.add("active");
-  })
-});
-
-
-// Works for class toggle
-// $(document).ready(function(){
-//     // Toggles span display
-//     $(".toggle-button").click(function(){
-//         $("span.transform").toggle();
-//     });
+// $('.adjective-1').click(function() {
+//     $(this).removeClass('hidden');
+//     $(this).addClass('active');
 // });
+
+
+function changeClass(active) {
+  var elem = document.getElementById('adjective-1');
+  elem.addClass('active');
+}
+
+
+// https://css-tricks.com/snippets/javascript/loop-queryselectorall-matches/
+// var forEach = function (array, callback, scope) {
+//   for (var i = 0; i < array.length; i++) {
+//     callback.call(scope, i, array[i]); // passes back stuff we need
+//   }
+// };
 //
-// $(document).ready(function(){
-//     // Toggles span display
-//     $(".toggle-button").click(function(){
-//         $("span.transform-2").toggle();
-//     });
+// var adjectives = document.querySelectorAll(".intro a.active");
+//
+// forEach(adjectives, function(i, el) {
+//   el.addEventListener("click", function() {
+//     document.querySelector(".intro a.active").classList.remove("active");
+//     this.classList.add("active");
+//   })
 // });
