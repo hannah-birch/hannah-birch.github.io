075843481231cef3661d3f72c7bf2bc951cff1d4
Hannah Birch <birch.hannah@gmail.com>
2020-04-09 21:49:16 -0400

    Starts styling buttons and spans

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -4,13 +4,13 @@
 // });
 $(document).ready(function () {
     $("#button-1").click(function () {
-        $(".hidden").switchClass("hidden", "active");
         $(".active").switchClass("active", "hidden");
+        $(".hidden").switchClass("hidden", "active");
     });
 
     $("#button-2").click(function () {
-        $(".active").switchClass("active", "hidden");
-        $(".hidden").switchClass("hidden", "active");
+      $(".hidden").switchClass("hidden", "active");
+      $(".active").switchClass("active", "hidden");
     });
 
     $("#button-3").click(function () {
