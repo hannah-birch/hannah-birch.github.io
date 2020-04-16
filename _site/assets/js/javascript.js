11a4acf51835922bfdfa3ecd09e0ca8b9d17bd4b
Hannah Birch <birch.hannah@gmail.com>
2020-04-16 09:22:58 -0400

    Switches from JS method to CSS, changes markup, starts styling words

--- _site/assets/js/javascript.js
+++ _site/assets/js/javascript.js
@@ -1,7 +1,4 @@
-// $('.adjective-1').click(function() {
-//     $(this).removeClass('hidden');
-//     $(this).addClass('active');
-// });
+
 $(document).ready(function () {
     $("#button-1").click(function () {
         $(".active").switchClass("active", "hidden");
@@ -18,20 +15,3 @@ $(document).ready(function () {
         $(".active").switchClass("active", "hidden-also");
     });
 });
-
-
-// https://css-tricks.com/snippets/javascript/loop-queryselectorall-matches/
-// var forEach = function (array, callback, scope) {
-//   for (var i = 0; i < array.length; i++) {
-//     callback.call(scope, i, array[i]); // passes back stuff we need
-//   }
-// };
-//
-// var adjectives = document.querySelectorAll(".intro a.active");
-//
-// forEach(adjectives, function(i, el) {
-//   el.addEventListener("click", function() {
-//     document.querySelector(".intro a.active").classList.remove("active");
-//     this.classList.add("active");
-//   })
-// });
